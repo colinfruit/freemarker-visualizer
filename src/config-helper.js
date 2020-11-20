f16e01b9baed50f30bd1f983106ed2bb5981f4d8
colinfruit <17092461+colinfruit@users.noreply.github.com>
2020-11-19 20:44:14 -0500

    add support for relative template paths

--- src/config-helper.js
+++ src/config-helper.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const getConfig = configPath => {
   try {
     return require(configPath)
@@ -10,8 +12,15 @@ const sanitizeConfig = config => {
   if (typeof config.directories === 'string') {
     config.directories = config.directories.split(',')
   }
+
   if (config.template) {
-    // config.template = path.resolve(config.template)
+    // enables the user to provide a template path relative to the cwd
+    const templatePath = path.resolve(config.template)
+    config.directories.forEach(dir => {
+      if (templatePath.includes(dir)) {
+        config.template = templatePath.split(dir).pop()
+      }
+    })
   }
   return config
 }

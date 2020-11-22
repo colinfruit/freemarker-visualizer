d88ac5c2c6b287e12f1c0c9cba5257ec1afe9a95
colinfruit <17092461+colinfruit@users.noreply.github.com>
2020-11-22 18:45:54 -0500

    0.1.0

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const filterObject = (obj) => {
   return filtered;
 };
 
-program.version('0.0.2');
+program.version('0.1.0');
 
 program
   .option('--directories [directories...]', 'FTL base directories')
